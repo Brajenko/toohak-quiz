c184baa4ed2459295467667aac108600fad19b21
poolwerx <nicovern@mail.ru>
2022-04-19 17:53:44 +0300

    first coomit / add files

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == 'POST':
+        print(request.form['email'])
         print(request.form['username'])
     return render_template('login_form.html')
 
